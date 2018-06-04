7cd29102da9ab1ce177677ca4427ce2229dd29c0
iblesa <isma.work@gmail.com>
2018-06-04 18:54:03 +0100

    Fixing adapter

--- app/src/main/java/com/iblesa/androidcook/master/MasterListIngredientsAdapter.java
+++ app/src/main/java/com/iblesa/androidcook/master/MasterListIngredientsAdapter.java
@@ -3,6 +3,7 @@ package com.iblesa.androidcook.master;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -24,7 +25,9 @@ public class MasterListIngredientsAdapter extends RecyclerView.Adapter<MasterLis
     @NonNull
     @Override
     public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new IngredientViewHolder(parent);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.master_ingredient_list_item, parent, false);
+        return new IngredientViewHolder(view);
     }
 
     @Override
