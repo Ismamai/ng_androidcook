245881e197f2c7a922f6b97216c22af69dc2284b
iblesa <isma.work@gmail.com>
2018-06-05 19:58:00 +0100

    Extract data from activity to display step

--- app/src/main/java/com/iblesa/androidcook/detail/DetailActivity.java
+++ app/src/main/java/com/iblesa/androidcook/detail/DetailActivity.java
@@ -3,7 +3,10 @@ package com.iblesa.androidcook.detail;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
+import com.iblesa.androidcook.Constants;
 import com.iblesa.androidcook.R;
 
 public class DetailActivity extends AppCompatActivity {
@@ -13,6 +16,15 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+
+    }
+
+    public void clickNext(View view) {
+        Log.d(Constants.TAG, "Click on button to go to Next Step");
+
+    }
+    public void clickPrevious(View view) {
+        Log.d(Constants.TAG, "Click on button to go to Previous Step");
 
     }
 }
--- app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
+++ app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
@@ -5,15 +5,22 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.iblesa.androidcook.Constants;
 import com.iblesa.androidcook.R;
 import com.iblesa.androidcook.model.Step;
 
 public class DetailFragment extends Fragment {
     private static final String STEP = "STEP";
+
+    private TextView mDescription;
+    private ImageView mMedia;
 
     public DetailFragment() {
     }
@@ -22,12 +29,21 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        mDescription = view.findViewById(R.id.step_desc);
+        mMedia = view.findViewById(R.id.step_media);
+
         Activity activity = getActivity();
+
         if (activity != null) {
             Intent intent = activity.getIntent();
 
             Step step = intent.getParcelableExtra(STEP);
+            mDescription.setText(step.getDescription());
+        } else {
+            Log.e(Constants.TAG, "Unable to retrieve intent STEP from activity");
         }
         return view;
     }
+
 }
