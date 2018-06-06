f519c41e141b7b2fddec2d1ab299f01451a6e121
iblesa <isma.work@gmail.com>
2018-06-06 22:11:34 +0100

    Adding picasso to the party

--- app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
+++ app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.iblesa.androidcook.Constants;
 import com.iblesa.androidcook.R;
 import com.iblesa.androidcook.model.Step;
+import com.squareup.picasso.Picasso;
 
 public class DetailFragment extends Fragment {
     private static final String STEP = "STEP";
@@ -37,9 +38,27 @@ public class DetailFragment extends Fragment {
 
         if (activity != null) {
             Intent intent = activity.getIntent();
-
             Step step = intent.getParcelableExtra(STEP);
             mDescription.setText(step.getDescription());
+            String videoUrl = step.getVideoURL();
+            if (videoUrl != null && !videoUrl.isEmpty()) {
+                Log.d(Constants.TAG, "VideoURL to load is " + videoUrl);
+            } else {
+
+                String thumbnailURL = step.getThumbnailURL();
+                Log.d(Constants.TAG, "ThumbnailURL is " + thumbnailURL);
+                if (thumbnailURL!=null && !thumbnailURL.isEmpty()) {
+                    Picasso.get()
+                            .load(thumbnailURL)
+                            .placeholder(R.drawable.image_placeholder)
+                            .error(R.drawable.image_placeholder)
+                            .into(mMedia);
+                } else {
+                    Picasso.get()
+                            .load(R.drawable.image_placeholder)
+                            .into(mMedia);
+                }
+            }
         } else {
             Log.e(Constants.TAG, "Unable to retrieve intent STEP from activity");
         }
