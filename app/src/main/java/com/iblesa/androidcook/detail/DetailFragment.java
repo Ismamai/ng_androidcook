d5dde736b8a105f5a1eb51d4f17d6e088b5392ce
iblesa <isma.work@gmail.com>
2018-06-09 22:31:27 +0100

    Adding exoplayer

--- app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
+++ app/src/main/java/com/iblesa/androidcook/detail/DetailFragment.java
@@ -1,6 +1,7 @@
 package com.iblesa.androidcook.detail;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -10,6 +11,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.iblesa.androidcook.Constants;
 import com.iblesa.androidcook.R;
 import com.iblesa.androidcook.model.Step;
@@ -18,6 +31,8 @@ import com.squareup.picasso.Picasso;
 public class DetailFragment extends Fragment {
 
     private Step mStep;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mSimpleExoPlayerView;
 
     public DetailFragment() {
     }
@@ -29,14 +44,17 @@ public class DetailFragment extends Fragment {
 
         TextView mDescription = view.findViewById(R.id.step_desc);
         ImageView mMedia = view.findViewById(R.id.step_media);
+        mSimpleExoPlayerView = view.findViewById(R.id.step_video);
 
         mDescription.setText(mStep.getDescription());
         String videoUrl = mStep.getVideoURL();
+        String thumbnailURL = mStep.getThumbnailURL();
+
         if (videoUrl != null && !videoUrl.isEmpty()) {
             Log.d(Constants.TAG, "VideoURL to load is " + videoUrl);
+            initializePlayer(Uri.parse(videoUrl));
         } else {
 
-            String thumbnailURL = mStep.getThumbnailURL();
             Log.d(Constants.TAG, "ThumbnailURL is " + thumbnailURL);
             if (thumbnailURL != null && !thumbnailURL.isEmpty()) {
                 Picasso.get()
@@ -54,6 +72,36 @@ public class DetailFragment extends Fragment {
         return view;
     }
 
+    /**
+     * Method to initialize player
+     *
+     * @param mediaUri mediaUri
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            //Create an instance of the ExoPlayer
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+
+            mSimpleExoPlayerView.setPlayer(mExoPlayer);
+            //Prepare the MEdiaSource
+            String userAgent = Util.getUserAgent(getActivity(), "AndroidCook");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getActivity(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    /**
+     * method to releasePlayer
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
     /**
      * Specifies the step to display in the detail activity
      * @param step Step to display
