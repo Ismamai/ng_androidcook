9483a1beb3229b29ba6627dfcf752d076690acee
iblesa <isma.work@gmail.com>
2018-06-28 19:56:03 +0100

    Implementing flaky test following this suggestion https://discussions.udacity.com/t/step-4-add-functional-tests-asynctask-test/487048/7

--- app/src/androidTest/java/com/iblesa/androidcook/SelectingRecipeTest.java
+++ app/src/androidTest/java/com/iblesa/androidcook/SelectingRecipeTest.java
@@ -1,5 +1,6 @@
 package com.iblesa.androidcook;
 
+import android.os.AsyncTask;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,6 +10,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -21,18 +26,47 @@ import static org.hamcrest.Matchers.anything;
 @RunWith(AndroidJUnit4.class)
 public class SelectingRecipeTest {
     public static final String NUTELLA_PIE = "Nutella Pie";
+    final CountDownLatch signal = new CountDownLatch(1);
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
     @Test
     public void showSelectedRecipeTest() {
-        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
-        // gridview item and clicks it.
-//        onData(anything()).inAdapterView(withId(R.id.rv_main_recipes)).atPosition(1).perform(click());
+        final AsyncTask task = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Log.d(Constants.TAG, "TEST: in AsyncTask waiting");
+
+                try {
+                    Log.d(Constants.TAG, "TEST: Got To sleep");
+
+                    Thread.sleep(5000L);
+                    Log.d(Constants.TAG, "TEST: Awake");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d(Constants.TAG, "TEST: signaling countdown");
+                signal.countDown();
+                return null;
+            }
+        };
+        task.execute();
+        Log.d(Constants.TAG, "TEST: Lets wait before we do the test");
+
+        try {
+            signal.await(10, TimeUnit.SECONDS);
+            Log.d(Constants.TAG, "TEST: We were signaled by the CountDownLatch");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d(Constants.TAG, "TEST: Starting the test");
 
         onView(withId(R.id.rv_main_recipes))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));        // Checks that the OrderActivity opens with the correct tea name displayed
 
         onView(withId(R.id.tv_master_recipe_name)).check(matches(withText(NUTELLA_PIE)));
+        Log.d(Constants.TAG, "TEST: Passed");
     }
 }
